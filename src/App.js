5e612281755c2974d5c744b05839743e6328ad9a
conlamon <connor.lamon@gmail.com>
2018-05-24 15:07:56 -0700

    Updated UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import SatelliteMap from './SatelliteMap.js';
-import Intro from './Intro';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Intro/>
           <SatelliteMap/>
       </div>
     );
--- src/SatelliteMap.js
+++ src/SatelliteMap.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { Map, TileLayer, FeatureGroup, Popup} from 'react-leaflet';
+import Control from 'react-leaflet-control';
 import { EditControl } from 'react-leaflet-draw';
 import './SatelliteMap.css';
 
@@ -10,23 +11,55 @@ class SatelliteMap extends Component {
         this.state = {
             markers: [],
             // Set the map starting point by Lat, Lng and Zoom
-            lat: 39.13,
-            lng: -121.63,
-            zoom: 11,
-            minZoom: 10,
-            maxZoom: 13,
+            lat: parseFloat(process.env.REACT_APP_STARTING_LATITUDE),
+            lng: parseFloat(process.env.REACT_APP_STARTING_LONGITUDE),
+            zoom: parseInt(process.env.REACT_APP_STARTING_ZOOM, 10),
+            minZoom: parseInt(process.env.REACT_APP_MIN_ZOOM, 10),
+            maxZoom: parseInt(process.env.REACT_APP_MAX_ZOOM, 10),
 
             // Set the bounds on the map to restrict user to where tiles are
-            northEastBound: [39.96177683325811, -120.29188071495460],
-            southWestBound: [37.83012627959109, -122.98198300864898],
+            northEastBound: [parseFloat(process.env.REACT_APP_NE_BOUND_LAT),
+                             parseFloat(process.env.REACT_APP_NE_BOUND_LNG)],
+            southWestBound: [parseFloat(process.env.REACT_APP_SW_BOUND_LAT),
+                             parseFloat(process.env.REACT_APP_SW_BOUND_LON)],
 
             // Setup the default values to display popup window on map
-            predictionList: [<tr key='waiting'><p>Waiting for the API to load...</p></tr>],
-            popupPosition: [39.13, -121.63]
+            predictionList: [<tr key='waiting'><p>Waiting for the API to load.<br/>
+                                                This may take up to 60 seconds due to the free Heroku Dyno shutting down :(,
+                                                Once, active, inference time is in the 100s ms range</p></tr>],
+            popupPosition: [parseFloat(process.env.REACT_APP_STARTING_LATITUDE),
+                            parseFloat(process.env.REACT_APP_STARTING_LONGITUDE)]
 
         }
     }
 
+
+    componentWillMount() {
+
+        // Pre-load the API on opening of this site (since the API goes to sleep with free hosting)
+        let coordsPayload = {
+            "lat1": this.state.popupPosition[0],
+            "lng1": this.state.popupPosition[1]
+        };
+        fetch(process.env.REACT_APP_API_URL, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(coordsPayload)
+        }).then(resp => {
+            if (!resp.ok) {
+                if (resp.status >= 400 && resp.status < 500) {
+                    return resp.json().then(data => {
+                        let err = {errorMessage: data.message};
+                        throw err;
+                    })
+                } else {
+                    let err = {errorMessage: 'Please try again later, server is not responding...'}
+                    throw err;
+                }
+            }
+            return resp.json();
+        })
+    }
 
     // Workhorse function for handling the event of clicking on a created polygon on the map
     onCreated(e) {
@@ -49,7 +82,7 @@ class SatelliteMap extends Component {
                };
 
                // make fetch POST call to the REST API with the lat, lng data
-                fetch('https://earth-classification-api.herokuapp.com/predict', {
+                fetch(process.env.REACT_APP_API_URL, {
                     method: 'POST',
                     headers: {'Content-Type':'application/json'},
                     body: JSON.stringify(coordsPayload)
@@ -83,12 +116,12 @@ class SatelliteMap extends Component {
 
                         // Set the data to be rendered in a table format
                          let predictionList = predictions.map((pred) => {
-                            return(
-                                <tr key={pred.label}>
-                                    <td>{pred.label}</td>
-                                    <td>{pred.probability.toFixed(2)}</td>
-                                </tr>
-                            )
+                                return(
+                                    <tr key={pred.label}>
+                                        <td>{pred.label}</td>
+                                        <td>{pred.probability.toFixed(2)}</td>
+                                    </tr>
+                                )
                          });
 
                          // Set the state
@@ -116,8 +149,6 @@ class SatelliteMap extends Component {
         const position = [this.state.lat, this.state.lng];
         const bounds = [this.state.northEastBound, this.state.southWestBound];
         return (
-            <div className='map-container'>
-                {/*Map element*/}
                 <Map
                     center={position}
                     zoom={this.state.zoom}
@@ -125,7 +156,8 @@ class SatelliteMap extends Component {
                     maxZoom={this.state.maxZoom}
                     minZoom={this.state.minZoom}>
                     <TileLayer
-                        attribution="<a href=http://www.esri.com/>Esri</a>"
+                        attribution="Imagery Courtesy USGS/NASA Landsat, <a href=http://www.esri.com/>Esri</a> and
+                        <a href=https://www.kaggle.com/c/planet-understanding-the-amazon-from-space/data/>Planet</a>"
                         url="http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
                     />
 
@@ -144,17 +176,45 @@ class SatelliteMap extends Component {
                             }}/>
 
                         {/*Popup to display the prediction results on the polygon*/}
-                        <Popup position={this.state.popupPosition}>
+                        <Popup position={this.state.popupPosition} className={'popup-container'}>
                             <table>
                                 <tbody>
+                                    <tr><td>Class:</td><td>Score:</td></tr>
                                     {this.state.predictionList}
                                 </tbody>
                             </table>
                         </Popup>
-
                     </FeatureGroup>
+
+                    <Control position="topright">
+                        <div className={'info-div'}>
+                            <p id={'info-header'} >
+                                <strong>Classify images of Earth in real-time! </strong>
+                                <br/>Thanks to Convolutional Neural Networks.
+                            </p>
+                            <p id={'info-body'}>
+                                <ol>
+                                    <li>Press the black square on the left side-bar</li>
+                                    <li>Draw a small rectangle on the map</li>
+                                    <li>Click on the drawn rectangle</li>
+                                    <li>Wait for classifications</li>
+                                    <li>Hows it work? <a href={'https://github.com/conlamon/varianceEarth'}>GitHub</a> </li>
+                                    <li>What are the labels? See 5.</li>
+                                    <li>Unconvinced? Scroll left to the <br/>large lake and try classifying it! Also, see 5.</li>
+                                </ol>
+
+                            </p>
+                        </div>
+                    </Control>
+                    <Control position="topright" >
+                        <div className={'info-div'}>
+                            <p>
+                                Note: The class 'clear' means <br/>
+                                there are no clouds in the image.
+                            </p>
+                        </div>
+                    </Control>
                 </Map>
-            </div>
 
         )
     }
